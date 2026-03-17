8916d5a94150638339a1ad1861c7acffa7431fd9
JMc Associates LLC <admin@jmcassociatesllc.com>
2026-03-17 16:01:50 -0400

    JMc - 2026-03-16 - Purged internal diagnostic and simulation files from the public repository matrix.

--- gemini-fs-bridge.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Total path override
-export PATH="/opt/homebrew/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:$PATH"
-# Use absolute path to npx directly
-exec /opt/homebrew/bin/npx -y @modelcontextprotocol/server-filesystem "$@"
